c51e998ef3efef616a927baab89df84f57505b3f
matsoz <55518409+matsoz@users.noreply.github.com>
2021-05-10 21:41:32 -0300

    1. Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var name = document.querySelector("#exampleInputName");
+
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#nameAdmin");
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function (field, index) {
+
+    console.log(field.name);
+
+});
